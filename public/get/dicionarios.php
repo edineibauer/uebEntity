72f633535f573aa4d87a3b81525dabe921c56c59
Edinei <galera.org@gmail.com>
2019-01-03 16:03:56 -0200

    get dicionario retorna dicionarios com referencia a colunas

--- public/get/dicionarios.php
+++ public/get/dicionarios.php
@@ -1,2 +1,13 @@
 <?php
-$data['data'] = Entity\Entity::dicionario();
+$dic = Entity\Entity::dicionario();
+
+//convert dicionário para referenciar colunas e não ids
+$dicionario = [];
+foreach ($dic as $entity => $metas) {
+    foreach ($metas as $i => $meta) {
+        $meta['id'] = $i;
+        $dicionario[$entity][$meta['column']] = $meta;
+    }
+}
+
+$data['data'] = $dicionario;
