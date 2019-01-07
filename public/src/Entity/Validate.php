a8a1cdcbbad374d2c722ec2744d8edf2f297c4d8
Edinei <galera.org@gmail.com>
2019-01-07 15:56:11 -0200

    remove loop in save value

--- public/src/Entity/Validate.php
+++ public/src/Entity/Validate.php
@@ -218,7 +218,7 @@ class Validate
         if ($m->getType() === "json" && is_array($m->getValue()))
             $m->setValue(json_encode($m->getValue()), false);
         elseif($m->getGroup() === "boolean")
-            $m->setValue($m->getValue() ? 1 : 0);
+            $m->setValue($m->getValue() ? 1 : 0, false);
 
         if ($m->getFormat() === "password" || $m->getFormat() === "passwordRequired") {
             $m->setValue(Check::password($m->getValue()), false);
