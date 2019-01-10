8c5b789f893f9c63aa20c9467deb2e1caac85fc1
Edinei <galera.org@gmail.com>
2019-01-10 17:49:49 -0200

    updates permissions

--- public/get/allow.php
+++ public/get/allow.php
@@ -1,5 +1,2 @@
 <?php
-
-$data['data'] = [];
-if(file_exists(PATH_HOME . "_config/permissoes.json"))
-    $data['data'] = json_decode(file_get_contents(PATH_HOME . "_config/permissoes.json"), true);
+$data['data'] = \Config\Config::getPermission();
