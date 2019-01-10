78b5cbac8dac4539714cbf5fce17defcab8f092e
Edinei <galera.org@gmail.com>
2019-01-10 17:38:59 -0200

    carregar entidades com verificação de permissão de leitura e owner

--- public/set/load/entity.php
+++ public/set/load/entity.php
@@ -3,23 +3,29 @@ $entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
 $time = filter_input(INPUT_POST, 'historic', FILTER_VALIDATE_INT);
 
 $setor = empty($_SESSION['userlogin']['setor']) ? 0 : $_SESSION['userlogin']['setor'];
-$entidades = \Config\Config::getEntityNotAllow();
+$permissoes = \Config\Config::getPermission();
 
 $json = new \Entity\Json();
 $hist = $json->get("historic");
 $data['data'] = [];
 
-if (\Entity\Entity::checkPermission($entity) && (empty($entidades[$setor]) || !in_array($entity, $entidades[$setor]))) {
+if (empty($entidades[$setor][$entity]) || $entidades[$setor][$entity]['read']) {
+    //Verifica se é multitenancy, se for, adiciona cláusula para buscar somente os dados referentes ao usuário
+    $info = \Entity\Metadados::getInfo($entity);
+    $where = null;
+    if(!empty($info['autor']) && $info['autor'] === 2)
+        $where = "WHERE ownerpub = " . $_SESSION['userlogin']['id'];
+
     //preenche caso não tenha nada de informação
     if (empty($hist[$entity])) {
         $hist[$entity] = strtotime('now');
         $json->save("historic", $hist);
     }
 
-    //verifica se há alterações nessa entidade que não forão recebidas pelo app
+    //verifica se há alterações nessa entidade que não forão recebidas pelo app, caso tenha, atualiza os dados
     if (empty($time) || $time < $hist[$entity]) {
         $read = new \Conn\Read();
-        $read->exeRead($entity);
+        $read->exeRead($entity, $where);
         $data['data']['data'] = $read->getResult() ?? [];
 
         //atualiza histórico local
