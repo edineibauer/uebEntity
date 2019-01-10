006f06018d693216b4b3529ca9a2fbb8e5869ff1
Edinei <galera.org@gmail.com>
2019-01-09 22:31:08 -0200

    desativa permissão temporáriamente até revisão

--- public/set/load/entity.php
+++ public/set/load/entity.php
@@ -1,60 +1,30 @@
 <?php
 $entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
-$time = filter_input(INPUT_POST, 'historic', FILTER_VALIDATE_INT, FILTER_REQUIRE_ARRAY);
+$time = filter_input(INPUT_POST, 'historic', FILTER_VALIDATE_INT);
 
-$setor = empty($_SESSION['userlogin']['setor']) ? 20 : $_SESSION['userlogin']['setor'];
+$setor = empty($_SESSION['userlogin']['setor']) ? 0 : $_SESSION['userlogin']['setor'];
 $entidades = \Config\Config::getEntityNotAllow();
-$read = new \Conn\Read();
 
 $json = new \Entity\Json();
 $hist = $json->get("historic");
 $data['data'] = [];
 
-if(empty($entity)) {
-    if($entitys = Entity\Entity::dicionario()) {
-        foreach ($entitys as $entity => $metas) {
-            if (\Entity\Entity::checkPermission($entity) && !empty($entidades[$setor]) && !in_array($entity, $entidades[$setor])) {
-
-                //verifica se há alterações nessa entidade que não forão recebidas pelo app
-                if(empty($hist[$entity]) || empty($time[$entity]) || $time[$entity] < $hist[$entity]){
-                    $read->exeRead($entity);
-                    $data['data']['data'][$entity] = $read->getResult();
-
-                    //preenche caso não tenha nada de informação
-                    if(empty($hist[$entity]))
-                        $hist[$entity] = strtotime('now');
-
-                    //atualiza variável local
-                    $time[$entity] = $hist[$entity];
-                }
-            }
-        }
-
-        $data['data']['historic'] = $time;
+if (\Entity\Entity::checkPermission($entity) && (empty($entidades[$setor]) || !in_array($entity, $entidades[$setor]))) {
+    //preenche caso não tenha nada de informação
+    if (empty($hist[$entity])) {
+        $hist[$entity] = strtotime('now');
         $json->save("historic", $hist);
-    } else {
-        $data['data'] = 0;
     }
 
-} else {
-    if (\Entity\Entity::checkPermission($entity) && !empty($entidades[$setor]) && !in_array($entity, $entidades[$setor])) {
+    //verifica se há alterações nessa entidade que não forão recebidas pelo app
+    if (empty($time) || $time < $hist[$entity]) {
+        $read = new \Conn\Read();
+        $read->exeRead($entity);
+        $data['data']['data'] = $read->getResult() ?? [];
 
-        //verifica se há alterações nessa entidade que não forão recebidas pelo app
-        if(empty($hist[$entity]) || empty($time[$entity]) || $time[$entity] < $hist[$entity]){
-            $read->exeRead($entity);
-            $data['data']['data'][$entity] = $read->getResult();
-
-            //preenche caso não tenha nada de informação
-            if(empty($hist[$entity])) {
-                $hist[$entity] = strtotime('now');
-                $json->save("historic", $hist);
-            }
-
-            //atualiza variável local
-            $time[$entity] = $hist[$entity];
-            $data['data']['historic'] = $time;
-        }
+        //atualiza histórico local
+        $data['data']['historic'] = $hist[$entity];
     } else {
-        $data['data'] = 0;
+        $data['data']['historic'] = 0;
     }
 }
