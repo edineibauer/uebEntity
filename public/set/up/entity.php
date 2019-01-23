3363b5daf14dbae0d187576947e8c7a0d4239c87
Edinei <galera.org@gmail.com>
2019-01-22 22:43:33 -0200

    up entity error control

--- public/set/up/entity.php
+++ public/set/up/entity.php
@@ -6,7 +6,11 @@ $dados = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY)
 if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
     $id = \Entity\Entity::add($entity, $dados);
     $data['data'] = [];
-    $data['data']['data'] = $id;
+
+    $read = new \Conn\Read();
+    $read->exeRead($entity, "WHERE id = :id", "id={$id}");
+    $data['data']['error'] = is_object($id) ? $id : 0;
+    $data['data']['data'] = ($read->getResult() ? $read->getResult()[0] : 0);
 
     $json = new \Entity\Json();
     $data['data']['historic'] = $json->get("historic")[$entity];
