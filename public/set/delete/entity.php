90ccac670eb898aaffe8e9a16766966c4d832a1f
Edinei <galera.org@gmail.com>
2019-01-25 11:04:22 -0200

    not allow null on publisher

--- public/set/delete/entity.php
+++ public/set/delete/entity.php
@@ -3,9 +3,9 @@
 $entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
 
-$result = \Entity\Entity::delete($entity, ['id' => $id]);
+\Entity\Entity::delete($entity, ['id' => $id]);
 $data['data'] = [];
-$data['data']['data'] = $result;
+$data['data']['data'] = 1;
 
 $json = new \Entity\Json();
 $data['data']['historic'] = $json->get("historic")[$entity];
