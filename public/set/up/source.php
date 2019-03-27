a4b7daea8e0c8a4e070e27768604ebe52447d745
Edinei J. Bauer <galera.org@gmail.com>
2019-03-27 17:27:12 -0300

    upload post FILE

--- public/set/up/source.php
+++ public/set/up/source.php
@@ -7,20 +7,27 @@ $fileType = strip_tags(trim(filter_input(INPUT_POST, 'fileType', FILTER_DEFAULT)
 $extensao = strip_tags(trim(filter_input(INPUT_POST, 'type', FILTER_DEFAULT)));
 $source = strip_tags(trim(filter_input(INPUT_POST, 'url', FILTER_DEFAULT)));
 
-if (!empty($source) && is_string($source) && preg_match('/;/i', $source)) {
+$isImage = preg_match('/^image/i', $fileType);
+$dir = "uploads/tmp/" . $name . "." . ($isImage ? 'webp' : $extensao);
+
+\Helpers\Helper::createFolderIfNoExist(PATH_HOME . "uploads");
+\Helpers\Helper::createFolderIfNoExist(PATH_HOME . "uploads/tmp");
+
+if(!empty($_FILES['upload'])) {
+    $file_data = $_FILES['upload'];
+    move_uploaded_file( $file_data['tmp_name'], PATH_HOME . $dir);
+
+} elseif (!empty($source) && is_string($source) && preg_match('/;/i', $source)) {
     list($type, $dd) = explode(';', $source);
     list(, $dd) = explode(',', $dd);
     $file_data = base64_decode(str_replace(' ', "+", $dd));
 
-    \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "uploads");
-    \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "uploads/tmp");
-
-    $isImage = preg_match('/^image/i', $fileType);
-    $dir = "uploads/tmp/" . $name . "." . ($isImage ? 'webp' : $extensao);
     file_put_contents(PATH_HOME . $dir, $file_data);
+}
 
+if(isset($file_data)) {
     $data['data']['url'] = HOME . $dir;
-    $data['data']['image'] = HOME . (preg_match('/^data:image\//i', $type) ? $dir : "assetsPublic/img/file.png");
+    $data['data']['image'] = HOME . ($isImage ? $dir : "assetsPublic/img/file.png");
     $data['data']['preview'] = "<img src='" . $data['data']['image'] . "' title='Imagem " . $name . "' class='left radius'/>";
 } else {
     $data['data']['url'] = "";
