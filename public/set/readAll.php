e5f60b9dcd21939c45b78dcd9705157bb747aa9d
Edinei J. Bauer <galera.org@gmail.com>
2019-04-07 12:25:54 -0300

    fix permission read

--- public/set/readAll.php
+++ public/set/readAll.php
@@ -1,12 +1,18 @@
 <?php
+
+use Config\Config;
+use \Conn\Read;
+
 $entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
 
 $data['data'] = 0;
 $setor = empty($_SESSION['userlogin']['setor']) ? 20 : $_SESSION['userlogin']['setor'];
-$entidades = \Config\Config::getEntityNotAllow();
 
-if (\Entity\Entity::checkPermission($entity) && !empty($entidades[$setor]) && !in_array($entity, $entidades[$setor])) {
-    $read = new \Conn\Read();
+$permissoes = Config::getPermission();
+$permissoes = isset($permissoes[$setor]) ? $permissoes[$setor] : [];
+
+if ($setor === "admin" || (!empty($permissoes[$entity]['read']) && $permissoes[$entity]['read'])) {
+    $read = new Read();
     $read->exeRead($entity);
     $data['data'] = $read->getResult();
 }
