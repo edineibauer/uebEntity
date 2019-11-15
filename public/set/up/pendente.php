ae65970be90bf741b009a7054b57bd00d5030b09
Edinei J. Bauer <galera.org@gmail.com>
2019-11-15 00:53:33 -0300

    check if have user

--- public/set/up/pendente.php
+++ public/set/up/pendente.php
@@ -1,27 +1,30 @@
 <?php
 
-$entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
-$dados = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+if(!empty($_SESSION['userlogin'])) {
+    $entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
+    $dados = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
 
-\Helpers\Helper::createFolderIfNoExist(PATH_HOME . '_cdn/sync');
-\Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/sync/" . $entity);
+    \Helpers\Helper::createFolderIfNoExist(PATH_HOME . '_cdn/sync');
+    \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/sync/" . $entity);
 
-$dir = PATH_HOME . "_cdn/sync/" . $entity . "/" . $_SESSION['userlogin']['id'] . ".json";
-if(!file_exists($dir)) {
+    $dir = PATH_HOME . "_cdn/sync/" . $entity . "/" . $_SESSION['userlogin']['id'] . ".json";
+    if (!file_exists($dir)) {
 
-    function recursiveJson($dados) {
-        foreach ($dados as $key => $dado) {
-            if(!empty($dado) && is_string($dado) && \Helpers\Check::isJson($dado)) {
-                $dados[$key] = str_replace('"', '\"', $dado);
-            } elseif(is_array($dado)) {
-                $dados[$key] = recursiveJson($dado);
+        function recursiveJson($dados)
+        {
+            foreach ($dados as $key => $dado) {
+                if (!empty($dado) && is_string($dado) && \Helpers\Check::isJson($dado)) {
+                    $dados[$key] = str_replace('"', '\"', $dado);
+                } elseif (is_array($dado)) {
+                    $dados[$key] = recursiveJson($dado);
+                }
             }
+
+            return $dados;
         }
 
-        return $dados;
+        $f = fopen($dir, "w+");
+        fwrite($f, json_encode(recursiveJson($dados)));
+        fclose($f);
     }
-
-    $f = fopen($dir, "w+");
-    fwrite($f, json_encode(recursiveJson($dados)));
-    fclose($f);
 }
