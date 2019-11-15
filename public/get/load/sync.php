b49c36bcb943b1cae8019de2824b868cb348f7a4
Edinei J. Bauer <galera.org@gmail.com>
2019-11-15 01:27:11 -0300

    sync info up and get

---
+++ public/get/load/sync.php
@@ -0,0 +1,10 @@
+<?php
+
+$dir = PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id'];
+
+if(file_exists($dir)) {
+    foreach (\Helpers\Helper::listFolder($dir) as $item) {
+        $data['data'][str_replace(".json", "", $item)] = json_decode(file_get_contents($dir . "/" . $item), !0);
+        unlink($dir . "/" . $item);
+    }
+}
--- public/set/up/sync.php
+++ public/set/up/sync.php
@@ -5,11 +5,10 @@ if(!empty($_SESSION['userlogin'])) {
     $dados = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
 
     \Helpers\Helper::createFolderIfNoExist(PATH_HOME . '_cdn/sync');
-    \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/sync/" . $entity);
+    \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id']);
 
-    $dir = PATH_HOME . "_cdn/sync/" . $entity . "/" . $_SESSION['userlogin']['id'] . ".json";
+    $dir = PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id'] . "/" . $entity . ".json";
     if (!file_exists($dir)) {
-
         function recursiveJson($dados)
         {
             foreach ($dados as $key => $dado) {
