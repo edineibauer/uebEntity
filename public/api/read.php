38f6bc929da778d5c70eeff4fd6f2f937dec7910
Edinei J. Bauer <galera.org@gmail.com>
2019-12-20 11:35:27 -0300

    apis get put login

---
+++ public/api/read.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Alias to get.php
+ */
+
+$var = explode("/", str_replace("read/", "", $_GET['data']));
+$entity = $var[0];
+if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
+
+    $result = [];
+    $limite = $var[1] ?? 100000000;
+    $offset = $var[2] ?? 0;
+
+    $read = new \Conn\Read();
+    $read->exeRead($entity, "LIMIT {$limite} OFFSET {$offset}");
+    if($read->getResult())
+        $result = $read->getResult();
+
+    $data['data'] = json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+} else {
+    $data = ['response' => 2, 'error' => 'entidade não existe'];
+}
