ef241446202abbdba448c52b8b8fa7ff7f2ce9c4
Edinei J. Bauer <galera.org@gmail.com>
2020-01-07 10:02:33 -0300

    fiz sync when not logged

--- public/get/load/sync.php
+++ public/get/load/sync.php
@@ -1,10 +1,12 @@
 <?php
 
-$dir = PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id'];
+if(!empty($_SESSION['userlogin'])) {
+    $dir = PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id'];
 
-if(file_exists($dir)) {
-    foreach (\Helpers\Helper::listFolder($dir) as $item) {
-        $data['data'][str_replace(".json", "", $item)] = json_decode(file_get_contents($dir . "/" . $item), !0);
-        unlink($dir . "/" . $item);
+    if (file_exists($dir)) {
+        foreach (\Helpers\Helper::listFolder($dir) as $item) {
+            $data['data'][str_replace(".json", "", $item)] = json_decode(file_get_contents($dir . "/" . $item), !0);
+            unlink($dir . "/" . $item);
+        }
     }
 }
