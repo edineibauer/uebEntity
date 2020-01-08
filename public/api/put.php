b122397eddca345653206e6a966df0b182a6048c
Edinei J. Bauer <galera.org@gmail.com>
2020-01-07 23:11:27 -0300

    remove alias da API

--- public/api/put.php
+++ public/api/put.php
@@ -1,15 +1,10 @@
 <?php
 
-/**
- * Alias to put.php
- */
-
 $var = explode("/", str_replace("put/", "", $_GET['data']));
 $entity = $var[0];
 if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
 
     $dados = $_POST;
-    $permission = \Config\Config::getPermission();
 
     if(!empty($dados['id']) && is_numeric($dados['id'])) {
         //update
@@ -17,13 +12,14 @@ if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
         /**
          * Se anonimo tiver permissão para leitura
          */
-        if ($permission[0][$entity]['update']) {
             $data['data'] = \Helpers\Helper::postRequest(HOME . "set/up/entity", $dados);
-        } else {
-            $data = ['response' => 2, 'error' => 'sem permissão de atualização para esta entidade'];
-        }
     } elseif(!empty($dados)) {
-        $data = ['response' => 2, 'error' => 'ID ausente para poder atualizar registro'];
+        //create
+
+        /**
+         * Se anonimo tiver permissão para leitura
+         */
+            $data['data'] = \Helpers\Helper::postRequest(HOME . "set/up/entity", $dados);
 
     } else {
         $data = ['response' => 2, 'error' => 'dados da entidade não foram submetidos via POST'];
---
+++ public/api/search.php
@@ -0,0 +1,27 @@
+<?php
+
+$var = explode("/", str_replace("search/", "", $_GET['data']));
+$entity = strip_tags(trim($var[0]));
+if (!empty($var[1])) {
+    $campo = strip_tags(trim($var[1]));
+    $busca = strip_tags(trim($var[2]));
+    if (file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
+
+        /**
+         * Se anonimo tiver permissão para leitura
+         */
+        $result = [];
+        $limite = $var[3] ?? 100000000;
+        $offset = $var[4] ?? 0;
+
+        $read = new \Conn\Read();
+        $read->exeRead($entity, "WHERE {$campo} LIKE '%{$busca}%'");
+        if ($read->getResult())
+            $result = $read->getResult();
+
+        $data['data'] = $result;
+
+    } else {
+        $data = ['response' => 2, 'error' => 'entidade não existe'];
+    }
+}
