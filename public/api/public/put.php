fc52d88383b96a899ba022c5a8dbf494bb824b55
Edinei J. Bauer <galera.org@gmail.com>
2020-02-06 17:24:56 -0300

    put api updated

--- public/api/public/put.php
+++ public/api/public/put.php
@@ -4,28 +4,27 @@ $var = explode("/", str_replace("put/", "", $_GET['data']));
 $entity = $var[0];
 if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
 
-    $dados = $_POST;
     $permission = \Config\Config::getPermission();
 
-    if(!empty($dados['id']) && is_numeric($dados['id'])) {
+    if(!empty($_POST['id']) && is_numeric($_POST['id'])) {
         //update
 
         /**
          * Se anonimo tiver permissão para leitura
          */
         if ($permission[0][$entity]['update'] || (!empty($_SESSION['userlogin']) && ($_SESSION['userlogin']['setor'] === "admin" || $permission[$_SESSION['userlogin']['setor']][$entity]['update']))) {
-            $data['data'] = [$entity => \Helpers\Helper::postRequest(HOME . "set/up/entity", $dados)];
+            $data['data'] = Entity\Entity::add($entity, $_POST);
         } else {
             $data = ['response' => 2, 'error' => 'sem permissão de atualização para esta entidade'];
         }
-    } elseif(!empty($dados)) {
+    } elseif(!empty($_POST)) {
         //create
 
         /**
          * Se anonimo tiver permissão para leitura
          */
         if ($permission[0][$entity]['create'] || (!empty($_SESSION['userlogin']) && ($_SESSION['userlogin']['setor'] === "admin" || $permission[$_SESSION['userlogin']['setor']][$entity]['create']))) {
-            $data['data'] = [$entity => \Helpers\Helper::postRequest(HOME . "set/up/entity", $dados)];
+            $data['data'] = Entity\Entity::add($entity, $_POST);
         } else {
             $data = ['response' => 2, 'error' => 'sem permissão de criação para esta entidade'];
         }
--- public/api/put.php
+++ public/api/put.php
@@ -2,21 +2,10 @@
 
 $var = explode("/", str_replace("put/", "", $_GET['data']));
 $entity = $var[0];
-if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
 
-    $dados = $_POST;
-
-    if(!empty($dados['id']) && is_numeric($dados['id'])) {
-        //update
-        $data['data'] = [$entity => \Helpers\Helper::postRequest(HOME . "set/up/entity", $dados)];
-    } elseif(!empty($dados)) {
-        //create
-        $data['data'] = [$entity => \Helpers\Helper::postRequest(HOME . "set/up/entity", $dados)];
-
-    } else {
-        $data = ['response' => 2, 'error' => 'dados da entidade não foram submetidos via POST'];
-    }
-
+if (file_exists(PATH_HOME . "entity/cache/{$entity}.json") && !empty($_POST)) {
+    //create or update
+    $data['data'] = Entity\Entity::add($entity, $_POST);
 } else {
-    $data = ['response' => 2, 'error' => 'entidade não existe'];
+    $data = ['response' => 2, 'error' => empty($_POST) ? "dados não foram recebidos via POST" : 'entidade não existe'];
 }
