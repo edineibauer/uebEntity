e6aa96e1dc6d77fe04e085d2c1eb65d180cee19d
Edinei J. Bauer <galera.org@gmail.com>
2020-07-01 10:27:24 -0300

    add create and update static function and remove loadData with filters

--- public/src/Entity/Entity.php
+++ public/src/Entity/Entity.php
@@ -23,6 +23,45 @@ class Entity extends EntityCreate
     }
 
     /**
+     * Create new entity data
+     *
+     * @param string $entity
+     * @param array $data
+     * @param bool $save
+     * @return mixed
+     */
+    public static function create(string $entity, array $data)
+    {
+        return self::exeCreate($entity, $data, !0);
+    }
+
+    /**
+     * Update entity data
+     *
+     * @param string $entity
+     * @param array $data
+     * @param bool $save
+     * @return mixed
+     */
+    public static function update(string $entity, array $data)
+    {
+        return self::exeCreate($entity, $data, !0);
+    }
+
+    /**
+     * Validate entity data
+     *
+     * @param string $entity
+     * @param array $data
+     * @param bool $save
+     * @return mixed
+     */
+    public static function validateData(string $entity, array $data)
+    {
+        return self::exeCreate($entity, $data, !1);
+    }
+
+    /**
      * Salva data à uma entidade
      *
      * @param string $entity
@@ -46,156 +85,6 @@ class Entity extends EntityCreate
     public static function delete(string $entity, $data, bool $checkPermission = true)
     {
         self::exeDelete($entity, $data, $checkPermission);
-    }
-
-    /**
-     * @param string $entity
-     * @param array $filter
-     * @param string|null $order
-     * @param bool $reverse
-     * @param int $limit
-     * @param int $offset
-     * @param string|null $historicFront
-     * @return mixed
-     */
-    public static function loadData(string $entity, array $filter = [], string $order = null, bool $reverse = false, int $limit = 1000, int $offset = -1, string $historicFront = null)
-    {
-        $limit = !empty($limit) && $limit > 0 ? $limit : (int)LIMITOFFLINE;
-        $historicFrontTime = (!empty($historicFront) ? (int)explode("-", $historicFront)[0] : 0);
-        $setor = (!empty($_SESSION['userlogin']) ? $_SESSION['userlogin']['setor'] : "0");
-        $permissoes = Config::getPermission();
-        $json = new Json();
-        $hist = $json->get("historic");
-        $result = ['historic' => 0];
-
-        /**
-         * Estou logado, não sou ADM, não tenho permissão de leitura, mas a entidade é o meu tipo de usuário
-         */
-        $entityIsMySetor = ($setor !== "admin" && (isset($permissoes[$setor][$entity]['read']) && !$permissoes[$setor][$entity]['read']) && $setor !== "0" && $entity === $setor);
-
-        if ($setor === "admin" || (isset($permissoes[$setor]) && isset($permissoes[$setor][$entity]) && isset($permissoes[$setor][$entity]['read']) && $permissoes[$setor][$entity]['read']) || $entityIsMySetor) {
-
-            //preenche caso não tenha nada de informação
-            if (empty($hist[$entity])) {
-                $hist[$entity] = strtotime('now') . "-" . rand(1000000, 9999999);
-                $json->save("historic", $hist);
-            }
-
-            $histTime = (int)explode("-", $hist[$entity])[0];
-
-            //verifica se há alterações nessa entidade que não forão recebidas pelo app, caso tenha, atualiza os dados
-            if (empty($historicFront) || ($historicFrontTime < $histTime && !file_exists(PATH_HOME . "_cdn/update/{$entity}/{$historicFront}.json"))) {
-                //download all data
-
-
-                $dicionario = Metadados::getDicionario($entity);
-                $info = Metadados::getInfo($entity);
-                $where = "WHERE id > 0";
-
-                // Verifica se existe um vinculo deste usuário com o conteúdo, se tiver busca também
-                if (!empty($setor) && $setor !== "admin" && $setor !== "0") {
-                    $metadados = Metadados::getDicionario($entity);
-
-                    $count = 0;
-                    foreach ($metadados as $col => $meta) {
-                        if ($meta['format'] === "list" && $meta['relation'] === $setor) {
-                            if ($count === 0)
-                                $where .= " && ({$meta['column']} = " . $_SESSION['userlogin']['setorData']['id'];
-                            else
-                                $where .= " || {$meta['column']} = " . $_SESSION['userlogin']['setorData']['id'];
-                            $count++;
-                        }
-                    }
-
-                    if ($count > 0)
-                        $where .= ")";
-                }
-
-                //Verifica se é multitenancy, se for, adiciona cláusula para buscar somente os dados referentes ao usuário
-                if ($where === "WHERE id > 0" && $setor !== "admin" && $setor !== "0" && !empty($info['autor']) && $info['autor'] === 2)
-                    $where .= " && ownerpub = " . $_SESSION['userlogin']['id'];
-
-                $filterResult = "";
-                if (!empty($filter))
-                    $filterResult = self::exeReadApplyFilter($filter, $dicionario);
-                $where .= $filterResult;
-
-                /**
-                 * Se não tiver permissão, mas for meus dados, permite
-                 */
-                if ($entityIsMySetor)
-                    $where .= " && id = " . $_SESSION['userlogin']['setorData']['id'];
-
-                $where .= " ORDER BY " . (!empty($order) ? $order : "id") . ($reverse === null || $reverse ? " DESC" : " ASC") . " LIMIT {$limit}" . (!empty($offset) && $offset > -1 ? " OFFSET " . ($offset + 1) : "");
-
-                $read = new Read();
-                $read->exeRead($entity, $where);
-                $results = $read->getResult() ?? [];
-                if (!empty($results)) {
-                    //obtém nome da coluna da senha
-                    if (!empty($info['password'])) {
-                        foreach ($dicionario as $idDicionario => $item) {
-                            if ($idDicionario == $info['password']) {
-                                $columnPassword = $item['column'];
-                                break;
-                            }
-                        }
-                    }
-
-                    foreach ($results as $i => $resultados) {
-                        $results[$i]['db_action'] = "create";
-
-                        //se tiver senha, exclui valor
-                        if (isset($columnPassword))
-                            $results[$i][$columnPassword] = null;
-                    }
-                }
-
-                $sql = new SqlCommand();
-                $sql->exeCommand("SELECT count(id) AS total FROM " . PRE . $entity . " WHERE id > 0" . $filterResult);
-                $result['total'] = $sql->getResult() && !empty($sql->getResult()[0]['total']) ? (int)$sql->getResult()[0]['total'] : 0;
-                $result['data'] = $results;
-                $result['tipo'] = 1;
-                $result['historic'] = $hist[$entity];
-
-            } elseif (!$entityIsMySetor && $historicFrontTime < $histTime) {
-
-                /**
-                 * Verifica se tem colunas relacionadas ao meu usuário, ao qual vinculam o registro ao meu usuário
-                 * se tiver, considera como registro meu
-                 */
-                $relationSetor = !1;
-                $relationSetorColumn = "";
-                if (!empty($setor) && $setor !== "admin" && $setor !== "0" && !empty($_SESSION['userlogin']['setorData']['id'])) {
-                    $metadados = Metadados::getDicionario($entity);
-                    foreach ($metadados as $col => $meta) {
-                        if ($meta['format'] === "list" && $meta['relation'] === $setor) {
-                            $relationSetor = !0;
-                            $relationSetorColumn = $meta['column'];
-                            break;
-                        }
-                    }
-                }
-
-                //download updates
-                $result['data'] = [];
-                $info = Metadados::getInfo($entity);
-                foreach (Helper::listFolder(PATH_HOME . "_cdn/update/{$entity}") as $update) {
-                    $historicUpdate = str_replace('.json', '', $update);
-                    $historicUpdateTime = (int)explode("-", $historicUpdate)[0];
-                    if ($historicFrontTime < $historicUpdateTime) {
-                        $dadosUp = json_decode(file_get_contents(PATH_HOME . "_cdn/update/{$entity}/{$update}"), !0);
-                        if (!empty($dadosUp) && ($setor === "admin" || empty($info['autor']) || ($relationSetor && $dadosUp[$relationSetorColumn] === $_SESSION['userlogin']['setorData']['id']) || ($info['autor'] === 2 && $dadosUp['ownerpub'] === $_SESSION['userlogin']['id'])))
-                            $result['data'][] = $dadosUp;
-                    }
-                }
-
-                $result['tipo'] = 2;
-                $result['historic'] = $hist[$entity];
-            }
-        }
-
-        return $result;
     }
 
     /**
@@ -384,76 +273,5 @@ class Entity extends EntityCreate
 
             return false;
         }
-    }
-
-    /**
-     * @param array $filter
-     * @param $dicionario
-     * @return string
-     */
-    private static function exeReadApplyFilter(array $filter, $dicionario)
-    {
-        $where = [];
-        foreach ($filter as $i => $filterOption) {
-
-            /**
-             * Converte valores caso esteja com os nomes em portugues
-             */
-            if (!isset($filterOption['operator']) && isset($filterOption['operador'])) {
-                $filterOption = [
-                    "column" => $filterOption['coluna'],
-                    "operator" => $filterOption['operador'],
-                    "value" => $filterOption['valor']
-                ];
-            }
-
-            if ($filterOption['operator'] === "por") {
-                foreach ($dicionario as $meta) {
-                    if (!in_array($meta['key'], ["information", "identifier"]))
-                        $where[$i][] = $meta['column'] . " LIKE '%{$filterOption['value']}%'";
-                }
-
-            } else {
-                switch ($filterOption['operator']) {
-                    case 'contém':
-                        $where[$i][] = "{$filterOption['column']} LIKE '%{$filterOption['value']}%'";
-                        break;
-                    case 'igual a':
-                        $where[$i][] = "{$filterOption['column']} = '{$filterOption['value']}'";
-                        break;
-                    case 'diferente de':
-                        $where[$i][] = "{$filterOption['column']} != '{$filterOption['value']}'";
-                        break;
-                    case 'começa com':
-                        $where[$i][] = "{$filterOption['column']} LIKE '{$filterOption['value']}%'";
-                        break;
-                    case 'termina com':
-                        $where[$i][] = "{$filterOption['column']} LIKE '%{$filterOption['value']}'";
-                        break;
-                    case 'maior que':
-                        $where[$i][] = "{$filterOption['column']} > {$filterOption['value']}";
-                        break;
-                    case 'menor que':
-                        $where[$i][] = "{$filterOption['column']} < {$filterOption['value']}";
-                        break;
-                    case 'maior igual a':
-                        $where[$i][] = "{$filterOption['column']} >= {$filterOption['value']}";
-                        break;
-                    case 'menor igual a':
-                        $where[$i][] = "{$filterOption['column']} <= {$filterOption['value']}";
-                }
-            }
-        }
-
-        $result = "";
-        foreach ($where as $andContainer) {
-            $result .= " && (";
-            foreach ($andContainer as $e => $or)
-                $result .= ($e > 0 ? " || " : "") . $or;
-
-            $result .= ")";
-        }
-
-        return $result;
     }
 }
