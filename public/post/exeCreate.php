7c5ba3d6ef731a9f01be17fd1580d45f682dbb17
Edinei J. Bauer <galera.org@gmail.com>
2020-08-17 18:37:06 -0300

    update hist to user on exeCreate, sync just 1 file

--- public/post/exeCreate.php
+++ public/post/exeCreate.php
@@ -5,19 +5,30 @@ $registro = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARR
 
 if (empty($entity))
     $data['error'] = "Entidade não foi informada";
-elseif(!file_exists(PATH_HOME . "entity/cache/{$entity}.json"))
+elseif (!file_exists(PATH_HOME . "entity/cache/{$entity}.json"))
     $data['error'] = "Entidade não existe";
-elseif(empty($registro))
+elseif (empty($registro))
     $data['error'] = "Dados não informados";
-elseif(!is_array($registro))
+elseif (!is_array($registro))
     $data['error'] = "Dados precisa ser um objeto";
 
-if(empty($data['error'])) {
+if (empty($data['error'])) {
     $data['data'] = \Entity\Entity::add($entity, $registro);
+
+    /**
+     * Update the historic for this user to not update the database on front
+     */
+    if (file_exists(PATH_HOME . "_cdn/store/historic.json")) {
+        $f = fopen(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$entity}.json", "w");
+        fwrite($f, json_decode(file_get_contents(PATH_HOME . "_cdn/store/historic.json"), !0)[$entity]);
+        fclose($f);
+    }
 
     /**
      * Check Error
      */
     if (!is_numeric($data['data']))
         $data['error'] = $data['data'];
+    else
+        $data['data'] = \Entity\Entity::exeRead($entity, $data['data'])[0];
 }
--- public/post/up/sync.php
+++ public/post/up/sync.php
@@ -5,25 +5,11 @@ if(!empty($_SESSION['userlogin'])) {
     $dados = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
 
     \Helpers\Helper::createFolderIfNoExist(PATH_HOME . '_cdn/sync');
-    \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id']);
 
-    $dir = PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id'] . "/" . $entity . ".json";
+    $dir = PATH_HOME . "_cdn/sync/" . $_SESSION['userlogin']['id'] . ".json";
     if (!file_exists($dir)) {
-        function recursiveJson($dados)
-        {
-            foreach ($dados as $key => $dado) {
-                if (!empty($dado) && is_string($dado) && \Helpers\Check::isJson($dado)) {
-                    $dados[$key] = str_replace('"', '\"', $dado);
-                } elseif (is_array($dado)) {
-                    $dados[$key] = recursiveJson($dado);
-                }
-            }
-
-            return $dados;
-        }
-
         $f = fopen($dir, "w+");
-        fwrite($f, json_encode(recursiveJson($dados)));
+        fwrite($f, json_encode($dados));
         fclose($f);
     }
 }
