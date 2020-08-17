e5c9447bee35854de84e14a29794c72ffdb2544b
Edinei J. Bauer <galera.org@gmail.com>
2020-08-17 12:03:13 -0300

    simple exeCreate request

---
+++ public/post/exeCreate.php
@@ -0,0 +1,23 @@
+<?php
+
+$entity = strip_tags(trim(filter_input(INPUT_POST, 'entity', FILTER_DEFAULT)));
+$registro = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+
+if (empty($entity))
+    $data['error'] = "Entidade não foi informada";
+elseif(!file_exists(PATH_HOME . "entity/cache/{$entity}.json"))
+    $data['error'] = "Entidade não existe";
+elseif(empty($registro))
+    $data['error'] = "Dados não informados";
+elseif(!is_array($registro))
+    $data['error'] = "Dados precisa ser um objeto";
+
+if(empty($data['error'])) {
+    $data['data'] = \Entity\Entity::add($entity, $registro);
+
+    /**
+     * Check Error
+     */
+    if (!is_numeric($data['data']))
+        $data['error'] = $data['data'];
+}
