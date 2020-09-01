003c75de066ae6ccca84bb6013a52f2cbcd6b5e7
Edinei J. Bauer <galera.org@gmail.com>
2020-09-01 17:15:32 -0300

    work with cache entity register

--- public/src/Entity/Entity.php
+++ public/src/Entity/Entity.php
@@ -31,12 +31,32 @@ class Entity extends EntityCreate
      */
     public static function exeRead(string $entity, $id = null)
     {
+        $json = new Json();
+        $historicEntity = $json->get("historic");
+
+        /**
+         * If not have a historic ID, create One
+         */
+        if (empty($historicEntity[$entity])) {
+            $historicEntity[$entity] = strtotime('now') . "-" . rand(1000000, 9999999);
+            $json->save("historic", $historicEntity);
+
+        } elseif(!$id && file_exists(PATH_HOME . "_cdn/storeEntityCache/{$entity}/{$historicEntity[$entity]}.json")) {
+
+            /**
+             * Check if have historic ID from this entity, if have
+             * check if the user historic ID is the same as the actual historic ID, if yes
+             * return the cached database
+             */
+            return json_decode(file_get_contents(PATH_HOME . "_cdn/storeEntityCache/{$entity}/{$historicEntity[$entity]}.json"), !0);
+        }
+
+        $result = [];
         $info = Metadados::getInfo($entity);
         $dicionario = Metadados::getDicionario($entity);
         $selects = "";
         $command = "FROM " . PRE . $entity . " as e";
         $relations = [];
-        $result = [];
         $dicionarios = [];
         $infos = [];
 
@@ -325,6 +345,23 @@ class Entity extends EntityCreate
                     }
                 }
             }
+
+            /**
+             * Clear all cache from this entity
+             */
+            if(file_exists(PATH_HOME . "_cdn/storeEntityCache/{$entity}")) {
+                foreach (Helper::listFolder(PATH_HOME . "_cdn/storeEntityCache/{$entity}") as $item)
+                    unlink(PATH_HOME . "_cdn/storeEntityCache/{$entity}/{$item}");
+            }
+
+            /**
+             * save the entity database to cache
+             */
+            Helper::createFolderIfNoExist(PATH_HOME . "_cdn/storeEntityCache");
+            Helper::createFolderIfNoExist(PATH_HOME . "_cdn/storeEntityCache/{$entity}");
+            $f = fopen(PATH_HOME . "_cdn/storeEntityCache/{$entity}/{$historicEntity[$entity]}.json", "w");
+            fwrite($f, json_encode($result));
+            fclose($f);
         }
 
         return $result;
