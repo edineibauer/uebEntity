f1de59e23f2ca09af99d31d5e1240391c4a50c9d
Edinei J. Bauer <galera.org@gmail.com>
2020-09-26 16:48:09 -0300

    create log change on CRUD

--- public/src/Entity/React.php
+++ public/src/Entity/React.php
@@ -47,13 +47,12 @@ class React
              */
             if ($action === "delete") {
                 $del = new Delete();
-                $del->exeDelete("wcache_" . $entity, "WHERE id = {$dados['id']}");
+                $del->exeDelete("wcache_" . $entity, "WHERE id = {$dadosOld['id']}");
             } else {
                 $this->updateCachedDatabase($entity, $dados, $action);
             }
 
-            //use this only if the sseEngineDB is using this
-            //$this->createUpdateSyncIndexedDb($action, $entity, $dados, $dadosOld);
+            $this->createUpdateSyncIndexedDb($action, $entity, $dadosOld['id']);
 
             /**
              * Include react for this operation if have
@@ -75,68 +74,44 @@ class React
     }
 
     /**
-     * Cria atualizações para serem sincronizadas com o front
-     * o front absorveria somente as alterações aqui informadas,
-     * não necessitando fazer o download de conteúdos não alterados
+     * Cria atualizações que dizem para o front o que ele deve receber de alteração
      *
      * @param string $action
      * @param string $entity
-     * @param array $dados
-     * @param array $old
+     * @param int $id
      */
-    private function createUpdateSyncIndexedDb(string $action, string $entity, array $dados, array $old)
+    private function createUpdateSyncIndexedDb(string $action, string $entity, int $id)
     {
-        //salva historico de alterações
-
         $list = Helper::listFolder(PATH_HOME . "_cdn/update/{$entity}");
-        $id = (!empty($list) ? (((int) str_replace(".json", "", explode("-", $list[count($list) -1])[1])) + 1) : 1);
+        $idHistoric = (!empty($list) ? (((int) str_replace(".json", "", explode("-", $list[count($list) -1])[1])) + 1) : 1);
 
         $json = new Json();
         $hist = $json->get("historic");
-        $hist[$entity] = strtotime('now') . "-" . $id;
+        $hist[$entity] = (string) strtotime('now') . "-" . $idHistoric;
         $json->save("historic", $hist);
 
         $store = new Json("update/{$entity}");
         $store->setVersionamento(!1);
-
-        $d = new Dicionario($entity);
-        foreach ($d->getDicionario() as $meta) {
-            if ($meta->getFormat() === "password")
-                $dados[$meta->getColumn()] = null;
-        }
-
-        $this->limitaAtualizaçõesArmazenadas($action, $entity, $dados);
-
-        if ($action === "delete") {
-            $store->save($hist[$entity], array_merge(['db_action' => "delete"], $dados));
-
-        } elseif ($action === "update") {
-            $store->save($hist[$entity], array_merge($old, ['db_action' => "update"], $dados));
-
-        } else {
-            $store->save($hist[$entity], array_merge(['db_action' => "create"], $dados));
-        }
+        $this->limitaAtualizaçõesArmazenadas($entity, $id);
+        $store->save($hist[$entity], ['db_action' => $action, "id" => $id]);
     }
 
     /**
-     * @param string $action
      * @param string $entity
-     * @param array $dados
+     * @param int $id
      */
-    private function limitaAtualizaçõesArmazenadas(string $action, string $entity, array $dados)
+    private function limitaAtualizaçõesArmazenadas(string $entity, int $id)
     {
         //remove updates anteriores de registros que serão excluídos
-        if ($action === "delete") {
-            foreach (\Helpers\Helper::listFolder(PATH_HOME . "_cdn/update/{$entity}") as $historie) {
-                $dadosE = json_decode(file_get_contents(PATH_HOME . "_cdn/update/{$entity}/{$historie}"), !0);
-                if ($dadosE['db_action'] !== "delete" && !empty($dadosE['id']) && !empty($dados['id']) && $dadosE['id'] == $dados['id'])
-                    unlink(PATH_HOME . "_cdn/update/{$entity}/{$historie}");
-            }
+        foreach (\Helpers\Helper::listFolder(PATH_HOME . "_cdn/update/{$entity}") as $historie) {
+            $dadosE = json_decode(file_get_contents(PATH_HOME . "_cdn/update/{$entity}/{$historie}"), !0);
+            if ($dadosE['db_action'] !== "delete" && !empty($dadosE['id']) && !empty($id) && $dadosE['id'] == $id)
+                unlink(PATH_HOME . "_cdn/update/{$entity}/{$historie}");
         }
 
         //se tiver mais que 100 resultados, deleta os acima de 100
         $lista = \Helpers\Helper::listFolder(PATH_HOME . "_cdn/update/{$entity}");
-        $limite = 100;
+        $limite = 200;
         if ((count($lista) + 1) > $limite) {
             $totalExcluir = count($lista) - 100;
             for ($i = 0; $i < $totalExcluir; $i++) {
